eef1e0fe9bf87a396e7950a6844e71e16d78114d
zhiming.sun <zhiming.sun@qq.com>
2023-10-12 16:34:02 +0800

    up

--- documents/.share/scripts/git-prompt.sh
+++ documents/.share/scripts/git-prompt.sh
@@ -1,69 +1,31 @@
-#!/usr/env/bin bash
+#!/usr/bin/env bash
 
-if test -f /etc/profile.d/git-sdk.sh
-then
-	TITLEPREFIX=SDK-${MSYSTEM#MINGW}
-else
-	TITLEPREFIX=$MSYSTEM
-fi
+__git_branch() {
+  local branch=$(git branch 2>/dev/null | grep '*' | sed 's/^* //g')
 
-if test -f ~/.config/git/git-prompt.sh
-then
-	. ~/.config/git/git-prompt.sh
-else
-	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
-	PS1="$PS1"'\n'                     # new line
+  if test -n "$branch"
+  then
+    echo -e "\033[36m($branch)\033[0m"
+  fi
+}
 
-	PS1="$PS1"'\033[34m'               # change color
-	PS1="$PS1"'# '                     #
-	PS1="$PS1"'\033[0m'                # reset color
+# color: blue
+PS1='\n\[\033[34m\]#\[\033[0m\] '
+# user. color: cyan
+PS1="$PS1"'\[\033[36m\]\u\[\033[0m\]'
+# '@'
+PS1="$PS1"' @ '
+# hostname. color: green
+PS1="$PS1"'\[\033[32m\]charlesbases\[\033[0m\]'
+# 'in'
+PS1="$PS1"' in '
+# $pwd. color: yellow
+PS1="$PS1"'\[\033[33m\]\w\[\033[0m\]'
+# time
+PS1="$PS1"' [\t] '
+# git-branch
+PS1="$PS1"'`__git_branch`'
+# '$'
+PS1="$PS1"'\n\[\033[31m\]$\[\033[0m\] '
 
-	PS1="$PS1"'\033[36m'               # change color
-	PS1="$PS1"'\u '					   # user
-	PS1="$PS1"'\033[0m'                # reset color
-
-	PS1="$PS1"'@ '                     #
-
-	PS1="$PS1"'\033[32m'               # change color
-	PS1="$PS1"'charlesbases '          # user
-	PS1="$PS1"'\033[0m'                # reset color
-
-	PS1="$PS1"'in '
-
-	PS1="$PS1"'\033[33m'               # change color
-	PS1="$PS1"'\w '                    # pwd
-	PS1="$PS1"'\033[0m'                # reset color
-
-	PS1="$PS1"'[\t]'
-
-	if test -z "$WINELOADERNOEXEC"
-	then
-		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
-		COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
-		COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
-		COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
-		if test -f "$COMPLETION_PATH/git-prompt.sh"
-		then
-			. "$COMPLETION_PATH/git-completion.bash"
-			. "$COMPLETION_PATH/git-prompt.sh"
-			PS1="$PS1"'\[\033[36m\]'  # change color to cyan
-			PS1="$PS1"'`__git_ps1`'   # bash function
-		fi
-	fi
-	PS1="$PS1"'\[\033[0m\]'        # change color
-	PS1="$PS1"'\n'                 # new line
-	PS1="$PS1"'\033[31m\$\033[0m '                 # prompt: always $
-fi
-
-MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
-
-# Evaluate all user-specific Bash completion scripts (if any)
-if test -z "$WINELOADERNOEXEC"
-then
-	for c in "$HOME"/bash_completion.d/*.bash
-	do
-		# Handle absence of any scripts (or the folder) gracefully
-		test ! -f "$c" ||
-		. "$c"
-	done
-fi
+MSYS2_PS1="$PS1"
